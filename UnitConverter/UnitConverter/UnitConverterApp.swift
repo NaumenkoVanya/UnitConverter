f5bf7c2fa6d80b767e3ebcffbbeb69d086de61c3
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-11 00:42:19 +0300

    Credit new project

---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Ваня Науменко on 11.02.24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
